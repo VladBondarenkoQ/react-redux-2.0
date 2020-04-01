5feeea068c7fe3887f11a66af372719cc6dc55a8
VladBondarenkoQ <jmot.train@gmail.com>
2020-04-01 21:15:14 +0200

    usersPage

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,21 +4,21 @@ import s from './Users.module.css'
 
 const Users = (props) => {
   return  <div>
-    {  props.users.map( u => <div key={u.id}>
-      <span>
+    {  props.users.map( u => <div className={s.wrapper} key={u.id}>
+      <div className={s.gridAreaA}>
         <div className={s.ava}><img src={u.photoUrl} /></div>
         <div>{u.followed ? <button className={s.followB} onClick={() => props.unfollow(u.id)}>Unfollow</button> : <button className={s.unfollowB} onClick={() => props.follow(u.id)}>Follow</button>}</div>
-      </span>
-      <span>
-        <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
-        </span>
-        <span>
-              <div>{u.location.country}</div>
+      </div>
+      
+        <div className={s.gridAreaB}>
+              <div className={s.fullName}>{u.fullName}</div>
+              <div className={s.status}>{u.status}</div>
+        </div>
+        <div className={s.gridAreaC}>
+              <div>{u.location.country},</div>
               <div>{u.location.city}</div>
-        </span>
-      </span>
+        </div>
+
       </div> )
     }
       </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -6,7 +6,7 @@ let initialState = {
   users:[ {fullName:'Joker', photoUrl:'https://i.pinimg.com/originals/85/dc/4e/85dc4e1241c7d76a97fa3da3a3ede637.jpg', id:1, location:{city:'Gotham', country:'USA'}, followed:false, status:'Im just kidding'},
           {fullName:'Alfred', photoUrl:'https://vignette.wikia.nocookie.net/earth279527/images/a/ad/Alfred_Pennyworth.jpg/revision/latest?cb=20170530030743', id:2, location:{city:'Gotham', country:'USA'}, followed:true, status:'I will gently tickle you with a mustache'},
           {fullName:'Robin',  photoUrl:'https://static0.srcdn.com/wordpress/wp-content/uploads/2020/02/Robert-Pattinson-in-The-Batman-Robin.jpg', id:3, location:{city:'Gotham', country:'USA'}, followed:true, status:'Harley Q, Im fallen in love'},
-          {fullName:'Punquin',  photoUrl:'https://i.pinimg.com/originals/15/ef/cf/15efcf357406457ee9849d7a842a87c2.jpg', id:4, location:{city:'Gotham', country:'USA'}, followed:false, status:'Losing weight'}]
+          {fullName:'Penquin',  photoUrl:'https://i.pinimg.com/originals/15/ef/cf/15efcf357406457ee9849d7a842a87c2.jpg', id:4, location:{city:'Gotham', country:'USA'}, followed:false, status:'Losing weight'}]
 }
 
 export const usersReducer = (state = initialState, action) => {
