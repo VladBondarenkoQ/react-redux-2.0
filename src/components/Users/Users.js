25d131e6f7f53e41cd0f59a41255d759e8617b93
VladBondarenkoQ <jmot.train@gmail.com>
2020-04-04 14:25:33 +0200

    53

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import s from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../images/0.jpg'
 
 
-const Users = (props) => {
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      this.props.setUsers(response.data.items)
+    })
+  }
+render() {
   return  <div>
-    {  props.users.map( u => <div className={s.wrapper} key={u.id}>
+
+    {  this.props.users.map( u => <div className={s.wrapper} key={u.id}>
       <div className={s.gridAreaA}>
-        <div className={s.ava}><img src={u.photoUrl} /></div>
-        <div>{u.followed ? <button className={s.followB} onClick={() => props.unfollow(u.id)}>Unfollow</button> : <button className={s.unfollowB} onClick={() => props.follow(u.id)}>Follow</button>}</div>
+        <div className={s.ava}><img src={userPhoto} /></div>
+        <div>{u.followed ? <button className={s.followB} onClick={() => this.props.unfollow(u.id)}>Unfollow</button> : <button className={s.unfollowB} onClick={() => this.props.follow(u.id)}>Follow</button>}</div>
       </div>
-      
+
         <div className={s.gridAreaB}>
-              <div className={s.fullName}>{u.fullName}</div>
+              <div className={s.fullName}>{u.name}</div>
               <div className={s.status}>{u.status}</div>
         </div>
         <div className={s.gridAreaC}>
-              <div>{u.location.country},</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'},</div>
+              <div>{'u.location.city'}</div>
         </div>
 
       </div> )
     }
       </div>
     }
+  }
 export default Users
